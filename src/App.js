b0699b278b6642e34fd5798fe74984bce10a01d9
YanaFedchenko <108426501+YanaFedchenko@users.noreply.github.com>
2022-11-13 19:10:21 -0800

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App 
+      Weather App
+      <footer>
+        This project was coded by Yana Fedchenko and is{" "}
+        <a href="https://github.com/YanaFedchenko/react-weather-app"
+          target="_blank">
+          open-sourse on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
