9ecf18639ef36e15ac32838d74801bc4382d7c8e
YanaFedchenko <108426501+YanaFedchenko@users.noreply.github.com>
2022-11-13 19:51:38 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Yana Fedchenko and is{" "}
-        <a href="https://github.com/YanaFedchenko/react-weather-app"
-          target="_blank">
-          open-sourse on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Yana Fedchenko and is{" "}
+          <a
+            href="https://github.com/YanaFedchenko/react-weather-app"
+            target="_blank"
+          >
+            open-sourse on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
