908fa88d76d7cece2d2f8921a88034d6aa07520a
YanaFedchenko <108426501+YanaFedchenko@users.noreply.github.com>
2022-11-22 08:07:20 -0800

    Displayed WeatherForecast one day

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -19,7 +19,7 @@ export default function WeatherInfo(props) {
             <div className="clearfix">
               <div className="float-start">
                 <WeatherIcon
-                  code={props.data.icon}
+                  code={props.data.icon} size={52}
                 />
               </div>
               <div className="float-start">
